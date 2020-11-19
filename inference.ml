7a79952ef33c194b96e2c1067a86a0890791f652
Yoann <yoanncouillec@gmail.com>
2020-11-19 13:22:13 +0000

    equality

--- inference.ml
+++ inference.ml
@@ -18,6 +18,9 @@ type typed_children =
 and typed_term =
   | TypedTerm of term * ttype * typed_children
 
+and constr =
+  | Equality of ttype * ttype
+
 let symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 let c = ref 0
 let gensym () = incr c ; Char.escaped (String.get symbols (!c-1))
@@ -41,6 +44,24 @@ let rec typed_term_of_term t = match t with
   | App(t1,t2) -> TypedTerm(t,TVar(gensym()),
                             TypedChildrenApp(typed_term_of_term t1,
                                              typed_term_of_term t2))
+
+let rec constr_of_typed_term term = match term with
+
+  | TypedTerm(Int(_),_,TypedChildrenInt) -> []
+
+  | TypedTerm(Var(_),_,TypedChildrenVar) -> []
+
+  | TypedTerm(Lambda(_,_),type1,TypedChildrenLambda(typed_term1,typed_term2)) ->
+     (match typed_term1, typed_term2 with
+      | (TypedTerm(_,type2,_), TypedTerm(_,type3,_)) ->
+         Equality(type1,TApp(type2,type3))::(constr_of_typed_term typed_term1)@(constr_of_typed_term typed_term2))
+
+  | TypedTerm(App(_,_),type1,TypedChildrenApp(typed_term1,typed_term2)) ->
+     (match typed_term1, typed_term2 with
+      | (TypedTerm(_,type2,_),TypedTerm(_,type3,_)) ->
+         Equality(type2,TApp(type3,type1))::(constr_of_typed_term typed_term1)@(constr_of_typed_term typed_term2))
+  | _ -> failwith "Parse error"
+
 (*************)
 (* STRING OF *)
 (*************)
@@ -76,6 +97,10 @@ and string_of_typed_children level = function
   | TypedChildrenLambda (tt1,tt2) -> (nspaces level)^"TypedChildrenLambda(\n"^(string_of_typed_term (level+1) tt1)^",\n"^(string_of_typed_term (level+1) tt2)^")"
   | TypedChildrenApp (tt1,tt2) -> (nspaces level)^"TypedChildrenApp(\n"^(string_of_typed_term (level+1) tt1)^",\n"^(string_of_typed_term (level+1) tt2)^")"
 
+
+let string_of_constr = function
+  | Equality(tt1,tt2) -> "Equality("^(string_of_type tt1)^", "^(string_of_type tt2)^")"
+
 (********)
 (* MAIN *)
 (********)
@@ -89,5 +114,6 @@ let _ =
        * Lambda(Var("x"),Var("x"));
        * Lambda(Var("x"),Lambda(Var("y"),Var("x"))); *)
     ] in
+  List.map (fun t -> List.map (fun c -> print_endline(string_of_constr c)) (constr_of_typed_term (typed_term_of_term t))) ts;
   (*List.map (fun t -> print_endline (string_of_typed_term 0 (typed_term_of_term t));print_newline()) ts*)
-  List.map (fun t -> List.map (function (t,tt) -> print_endline((string_of_term2 t)^" : "^(string_of_type tt))) (term_type_of_term t)) ts
+  (*List.map (fun t -> List.map (function (t,tt) -> print_endline((string_of_term2 t)^" : "^(string_of_type tt))) (term_type_of_term t)) ts*)
