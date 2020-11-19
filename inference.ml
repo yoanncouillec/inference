9a1ccac2925a12cbb75fe6deed6acc7a53a0ab0c
Yoann <yoanncouillec@gmail.com>
2020-11-19 08:46:55 +0000

    typed children

--- inference.ml
+++ inference.ml
@@ -25,36 +25,51 @@ let gensym () = incr c ; Char.escaped (String.get symbols (!c-1))
 let rec typed_term_of_term t = match t with
   | Int(_) -> TypedTerm (t,TInt,TypedChildrenInt)
   | Var(_) -> TypedTerm (t, TVar(gensym()), TypedChildrenVar)
-  | Lambda(Var(s),t) -> TypedTerm(t, TypedLambda(TypedVar(s, TVar(gensym())),
-                                     typed_term_of_term t,
-                                     TVar(gensym()))
+  | Lambda(Var(s),body) -> TypedTerm(t, TVar(gensym()),
+                                  TypedChildrenLambda(typed_term_of_term(Var s),
+                                                      typed_term_of_term(body)))
   | Lambda (_,_) -> failwith "Parse error"
-  | App(t1,t2) -> TypedApp(typed_term_of_term t1, typed_term_of_term t2,TVar(gensym()))
+  | App(t1,t2) -> TypedTerm(t,TVar(gensym()),
+                            TypedChildrenApp(typed_term_of_term t1,
+                                             typed_term_of_term t2))
+
+(* STRING OF *)
 
 let rec nspaces = function
   | 0 -> ""
   | n -> "   "^(nspaces (n-1))
 
-let rec string_of_term = function
-  | Int(n) -> "Int("^(string_of_int n)^")"
-  | Var(s) -> "Var("^s^")"
-  | Lambda(t1,t2) -> "Lambda("^(string_of_term t1)^", "^(string_of_term t2)^")"
-  | App(t1, t2) -> "App("^(string_of_term t1)^", "^(string_of_term t2)^")"
+let rec string_of_term level = function
+  | Int(n) -> (nspaces level)^"Int("^(string_of_int n)^")"
+  | Var(s) -> (nspaces level)^"Var("^s^")"
+  | Lambda(t1,t2) -> (nspaces level)^"Lambda(\n"^(string_of_term (level+1) t1)^",\n"^(string_of_term (level+1) t2)^")"
+  | App(t1, t2) -> (nspaces level)^"App(\n"^(string_of_term (level+1) t1)^",\n"^(string_of_term (level+1) t2)^")"
 
 let rec string_of_type = function
-  | TInt -> "int"
-  | TVar(s) -> s
-  | TApp(t1,t2) -> (string_of_type t1)^" -> "^(string_of_type t2)
+  | TInt -> "TInt"
+  | TVar(s) -> "TVar("^s^")"
+  | TApp(t1,t2) -> "TApp("^(string_of_type t1)^", "^(string_of_type t2)^")"
 
 let rec string_of_typed_term level = function
-  | TypedInt(n,t) -> (nspaces level)^"TypedInt("^(string_of_int n)^", "^(string_of_type t)^")"
-  | TypedVar(s,t) -> (nspaces level)^"TypedVar("^s^", "^(string_of_type t)^")"
-  | TypedLambda(tt1,tt2,t) -> (nspaces level)^"TypedLambda(\n"^(string_of_typed_term (level+1) tt1)^",\n"^(string_of_typed_term (level+1) tt2)^",\n"^(nspaces (level+1))^(string_of_type t)^")"
-  | TypedApp(tt1,tt2,t) -> (nspaces level)^"TypedApp(\n"^(string_of_typed_term (level+1) tt1)^",\n"^(string_of_typed_term (level+1) tt2)^",\n"^(nspaces (level+1))^(string_of_type t)^")"
+  | TypedTerm(t,tt,tc) ->
+     (nspaces level)^"TypedTerm(\n"^(string_of_term (level+1) t)^", "^(string_of_type tt)^",\n"^(string_of_typed_children (level+1) tc)^")"
+
+and string_of_typed_children level = function
+  | TypedChildrenInt -> (nspaces level)^"TypedChildrenInt"
+  | TypedChildrenVar -> (nspaces level)^"TypedChildrenVar"
+  | TypedChildrenLambda (tt1,tt2) -> (nspaces level)^"TypedChildrenLambda(\n"^(string_of_typed_term (level+1) tt1)^",\n"^(string_of_typed_term (level+1) tt2)^")"
+  | TypedChildrenApp (tt1,tt2) -> (nspaces level)^"TypedChildrenApp(\n"^(string_of_typed_term (level+1) tt1)^",\n"^(string_of_typed_term (level+1) tt2)^")"
+
+(* MAIN *)
 
 let _ =
-  let t1 = App(App(Lambda(Var "x",Lambda(Var "y",Var("y"))),Int(2)),Int(1)) in
-  let tt1 = TApp(TVar("a"),TVar("b")) in
-  print_endline (string_of_term t1);
-  print_endline (string_of_type tt1);
-  print_endline (string_of_typed_term 0 (typed_term_of_term t1));
+  (*let t1 = App(App(Lambda(Var "x",Lambda(Var "y",Var("y"))),Int(2)),Int(1)) in*)
+  let ts = [
+      App(App(Lambda(Var "x",Lambda(Var "y",Var("y"))),Int(2)),Int(1));
+      (* Var("x");
+       * Int(12);
+       * Lambda(Var("x"),Var("x"));
+       * Lambda(Var("x"),Lambda(Var("y"),Var("x"))); *)
+    ] in
+  List.map (fun t -> print_endline (string_of_typed_term 0 (typed_term_of_term t));print_newline()) ts
+
